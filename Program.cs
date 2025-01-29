e22e5d47cda4531deafccc94fe9eb104f47b93ee
l.caenazzo <l.caenazzo@aizoongroup.com>
2025-01-29 14:44:48 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -11,6 +11,43 @@ using Message = Telegram.Bot.Types.Message;
 // ReSharper disable once CheckNamespace
 class Program
 {
+    private static readonly string BotToken = "7997826290:AAGdFuQNlwjynaheYTV6wq7kBlYr5WBWNQw";
+    private static readonly long AmChatId = -1002381222429;
+    private static readonly string MalePoliceEmoji = "\U0001F46E\u200D\u2642\U0000FE0F"; // 👮‍♂️
+
+    static readonly List<string> Remarks =
+    [
+        "messo il turbo oggi, eh?",
+        "sei un podcast vivente ma hai rotto il cazzo",
+        "stai facendo il monologo finale di un film, o c’è una pausa da qualche parte?",
+        "vuoi un microfono, o ti senti già abbastanza amplificato?",
+        "sei già al capitolo 3 del tuo libro di puttanate?",
+        "minchia oh, non ci sei solo tu qua eh",
+        "forse è il momento di passare la parola agli altri.",
+        "vai a giocare con la merda nella tundra.",
+        "mi piacerebbe sentire anche il punto di vista di qualcun altro di voi stronzetti.",
+        "grazie per la tua passione, ma non hai un cazzo da fare oggi?",
+        "puoi riassumere? Abbiamo poco tempo per leggere tutte le cagate che scrivi.",
+        "hai coperto ogni dettaglio delle tue minchiate, possiamo passare al prossimo argomento?",
+        "facciamo un break dalle minchiate, che dici?",
+        "va bene, ho capito. Possiamo chiudere il discorso qui che hai scardinato lo scroto?",
+        "ma smettila di fare il gazzabbubbo di turno, che qui non siamo al circo!",
+        "se continui a parlare così, finisci dritto dritto nel manuale del perfetto spruzzafuffa.",
+        "ma sei proprio un mestolone di gorgoglione oggi, eh?",
+        "oh, gazzabbubbo ufficiale, la parola la passiamo anche agli altri o no?",
+        "sembri un frastugliacazzi, vai avanti all’infinito!",
+        "basta con questa manfrina da scatafasco ambulante!",
+        "ma quanto hai bevuto dal calderone della logorrina oggi?",
+        "Hai intenzione di brevettare questa infinita marea di cagate?",
+        "Va bene, ho capito, il mondo ruota intorno alla tua voce oggi!",
+        "Non ti stanchi mai di sentire il suono delle tue stesse stronzate?",
+        "Aspetta, fammi respirare tra una frase e l’altra minchia!",
+        "Se parlassi un po’ di meno, potremmo forse risolvere questo problema prima di domani.",
+        "Se le parole fossero denaro, potresti comprarti un biglietto per andare affanculo.",
+        "Mi fai sentire come se stessi leggendo il manuale di un elettrodomestico.",
+        "Hai già conquistato il trofeo del scassacazzi dell’anno, possiamo passare oltre?"
+    ];
+
     static async Task Main(string[] args)
     {
         string botToken = "7997826290:AAGdFuQNlwjynaheYTV6wq7kBlYr5WBWNQw";
